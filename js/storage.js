d23ddf212e10c85c63bbe8415bc4c4ad215a950c
noora-na2307714 <na2307714@qu.edu.qa>
2026-03-23 16:01:23 +0300

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 function getUsers() {
     return JSON.parse(localStorage.getItem("users")) || [];
 }
@@ -14,4 +9,34 @@ function saveUsers(users) {
 function getCurrentUserData() {
     return JSON.parse(localStorage.getItem("currentUser"));
 }
+function getPosts() {
+    return JSON.parse(localStorage.getItem("posts")) || [];
+}
+function getCurrentUserData() {
+    return JSON.parse(localStorage.getItem("currentUser"));
+}
 
+function refreshCurrentUser() {
+    const currentUser = getCurrentUserData();
+    if (!currentUser) return null;
+
+    const users = getUsers();
+    const freshUser = users.find(user => user.id === currentUser.id);
+
+    if (freshUser) {
+        localStorage.setItem("currentUser", JSON.stringify(freshUser));
+        return freshUser;
+    }
+
+    return currentUser;
+}
+
+function formatDate(dateString) {
+    return new Date(dateString).toLocaleString();
+}
+
+function escapeHtml(text) {
+    const div = document.createElement("div");
+    div.textContent = text;
+    return div.innerHTML;
+}
