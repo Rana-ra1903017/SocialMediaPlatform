f4fc095a5c0e69173039dd6dea5c3d3556c4e4c0
Rana-ra1903017 <ranaaldosari6@gmail.com>
2026-05-02 09:30:36 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,9 @@
 import "./globals.css";
 export const metadata = {
   title: "SocialHub",
-  description: "Simple Phase 2 project",
+  description: "Social Media Platform Web Phase 2 project",
 };
-export default function RootLayout({ children }) {
+export default function RootLayout({children}) {
   return (
     <html lang="en">
       <body>{children}</body>
